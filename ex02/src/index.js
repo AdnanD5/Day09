cfdc6b0b771fe3a83b4ff31e38906b0a04d95f55
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-16 13:44:27 +0100

    Dodavanje ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+//Only change code below line
+function myFunction(myParameter){
+if(myParameter === true){
+    return "The parameter is true!";
+}else{
+    return "The parameter is false!";
+}
+}
+console.log(myFunction(true));
+console.log(myFunction(false));
+//Only change code above line
+module.exports = myFunction;
